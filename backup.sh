7f56c7db2498f3e82e9b8ebf216bd0567cb7f45c
Kay <i@v2bv.net>
2018-04-20 21:36:44 -0700

    Add sync command

--- backup.sh
+++ backup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+SYNC_COMMAND="/usr/bin/rclone sync /opt/backup GDrive:/server-backup";
+
 set -e;
 
 # GitLab
 /opt/gitlab/bin/gitlab-rake gitlab:backup:create CRON=1;
 cp -R /etc/gitlab /opt/backup/gitlab/config;
+
+# Sync backups
+$SYNC_COMMAND;
