b5fb6a2b345fb7cd1d01a9be5f760db28131d97b
Kay <i@v2bv.net>
2018-04-21 18:54:25 -0700

    Add function copy.

--- backup.sh
+++ backup.sh
@@ -4,13 +4,20 @@ SYNC_COMMAND="/usr/bin/rclone sync /opt/backup GDrive:/server-backup";
 
 set -e;
 
+function copy {
+	if [ ! -d $2 ]; then
+		mkdir -p $2;
+	fi
+	cp -R $1 $2; 
+}
+
 # GitLab
 /opt/gitlab/bin/gitlab-rake gitlab:backup:create CRON=1;
-cp -R /etc/gitlab /opt/backup/gitlab/config;
+copy /etc/gitlab /opt/backup/gitlab/config;
 
 # Mastodon
 sudo -u postgres pg_dumpall | gzip /opt/backup/pg_dump.gz;
-cp -R /home/mastodon/live /opt/backup/mastodon;
+copy /home/mastodon/live /opt/backup/mastodon;
 
 # Sync backups
 $SYNC_COMMAND;
