db2148b591c7898c65beab4c33aef2b3d8da0c42
Kay <i@v2bv.net>
2018-04-21 18:44:46 -0700

    Add Mastodon

--- backup.sh
+++ backup.sh
@@ -8,5 +8,9 @@ set -e;
 /opt/gitlab/bin/gitlab-rake gitlab:backup:create CRON=1;
 cp -R /etc/gitlab /opt/backup/gitlab/config;
 
+# Mastodon
+sudo -u postgres pg_dumpall | gzip /opt/backup/pg_dump.gz;
+cp -R /home/mastodon/live /opt/backup/mastodon;
+
 # Sync backups
 $SYNC_COMMAND;
